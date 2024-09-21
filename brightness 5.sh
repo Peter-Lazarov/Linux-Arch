fa465ee5063a95c932ab051b28477c7ad6319cd3
Peter Lazarov <peter_lazarov@hotmail.com>
2024-09-21 10:38:37 +0300

    Create brightness 5.sh

---
+++ brightness 5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# xrandr -q
+xrandr --output DVI-D-0 --brightness .5
+xrandr --output DVI-D-1 --brightness .5
+xrandr --output DVI-D-2 --brightness .5
+xrandr --output DP-1 --brightness 0.5
+
+kill -9 $PPID
