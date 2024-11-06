0737448169b8b4a1e7c50530854cdefc71843e32
Peter-Lazarov <peter_lazarov@hotmail.com>
2024-11-06 19:07:55 +0200

    add command for information about window position and size

--- open and move.sh
+++ open and move.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 xdg-open /home/peter/Documents/
 sleep 1s
-wmctrl -r 'Documents' -e 0,0,0,800,600
+wmctrl -r 'Documents' -e 0,28,0,799,571
 
 xdg-open /home/peter/Documents/_JavaScript/
 sleep 1s
-wmctrl -r '_JavaScript' -e 0,800,0,800,600
+wmctrl -r '_JavaScript' -e 0,801,28,799,571
 
 xdg-open /home/peter/Downloads/
 sleep 1s
-wmctrl -r 'Downloads' -e 0,0,600,800,600
+wmctrl -r 'Downloads' -e 0,0,628,799,540
 
 #xdg-open /home/peter/Downloads/
 #sleep 1s
-#wmctrl -r 'Downloads' -e 0,800,600,800,600
+#wmctrl -r 'Downloads' -e 0,801,628,799,540
+
+
+
+#sudo pacman -S xorg-xwininfo
+#xwininfo
